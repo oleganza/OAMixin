9cea9300e37a5774bc9a76696048f1a79c3653c1
Oleg Andreev <oleganza@gmail.com>
2009-12-26 23:10:59 +0100

    mixin attempt

--- OAMixin.m
+++ OAMixin.m
@@ -11,13 +11,43 @@
 
 @implementation OAMixin
 
+
+BOOL OAMixin_identifyingMethod() { return YES; }
+
 + (void) addToClass:(Class) klass
 {
   NSLog(@"Adding mixin %@ to class %@", self, klass);
-  // TODO: create a new subclass for a klass's superclass
+  SEL mixinIdentifier = sel_registerName([[NSString stringWithFormat:@"containsOAMixin_%s", 
+                                           class_getName(self)] 
+                                          cStringUsingEncoding:NSUTF8StringEncoding]);
+  if (class_respondsToSelector(klass, mixinIdentifier)) return;
+  Class superclass = class_getSuperclass(klass);
+  const char *name = [[NSString stringWithFormat:@"%s_mixed_to_%s", 
+                       class_getName(self), 
+                       class_getName(superclass)] 
+                        cStringUsingEncoding:NSUTF8StringEncoding];
+  Class mixedInClass = objc_allocateClassPair(superclass, name, 0);
+  class_addMethod(mixedInClass, mixinIdentifier, (IMP) OAMixin_identifyingMethod, "v@:");
   // TODO: copy ivars, methods, properties and class methods to this subclass
-  // TODO: switch klass's superclass to the mixin
+  unsigned int methodsCount;
+  Method* methods = class_copyMethodList(self, &methodsCount);
+  Method method;
+  unsigned int index = 0;
+  while (index < methodsCount)
+  {
+    method = methods[index++];
+    NSLog(@"Adding method %s from mixin %@", method_getName(method), self);
+    class_addMethod(mixedInClass, 
+                    method_getName(method), 
+                    method_getImplementation(method), 
+                    method_getTypeEncoding(method));
+  }
+  free(methods);
   
+  objc_registerClassPair(mixedInClass);
+  // Damn, class_setSuperclass is removed in ObjC 2.0
+  // TODO: come up with some smart idea
+//  class_setSuperclass(klass, mixedInClass);
 }
 
 + (void) initializeMixinsForClass: (Class) cls
@@ -33,8 +63,8 @@
     const char* name = protocol_getName(protocol);
     NSLog(@"Class %@ has a protocol %s", cls, name);
     Class mixin = objc_getClass(name);
-    Class mixinmeta = objc_getMetaClass(name);
-    if (mixin && mixinmeta && 
+    Class mixinmeta;
+    if (mixin && (mixinmeta = objc_getMetaClass(name)) && 
         class_respondsToSelector(mixinmeta, @selector(isSubclassOfClass:)) && 
         [mixin isSubclassOfClass:[OAMixin class]])
     {
